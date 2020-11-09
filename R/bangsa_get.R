b2d93712d4c6372fa59f61234b972cf177cd2f05
Ernest Guevarra <ernest@guevarra.io>
2020-11-09 03:50:14 +0000

    create bangsa_get functions

--- R/bangsa_get.R
+++ R/bangsa_get.R
@@ -17,6 +17,7 @@
 #' @examples
 #' bangsa_ls()
 #'
+#' @rdname bangsa_ls
 #' @export
 #'
 #
@@ -34,8 +35,140 @@ bangsa_ls <- function(id = "1W8JV4hth_gs1V6efb92HMz0eU2LRYbOO") {
 }
 
 
+################################################################################
+#
+#'
+#' @rdname bangsa_ls
+#' @export
+#'
+#
+################################################################################
+
+bangsa_ls_geodata <- function(id = bangsa_ls()$id[bangsa_ls()$name == "1-Geodata"]) {
+  ## Google Drive deauthorisation
+  googledrive::drive_deauth()
+
+  ## Get id
+  x <- bangsa_ls(id = id)
+
+  ## Return x
+  return(x)
+}
 
 
+################################################################################
+#
+#'
+#' @rdname bangsa_ls
+#' @export
+#'
+#
+################################################################################
 
+bangsa_ls_tabular <- function(id = bangsa_ls()$id[bangsa_ls()$name == "2-Tabular Data"]) {
+  ## Google Drive deauthorisation
+  googledrive::drive_deauth()
+
+  ## Get id
+  x <- bangsa_ls(id = id)
+
+  ## Return x
+  return(x)
+}
+
+
+################################################################################
+#
+#'
+#' @rdname bangsa_ls
+#' @export
+#'
+#
+################################################################################
+
+bangsa_ls_docs <- function(id = bangsa_ls()$id[bangsa_ls()$name == "3-Development Plans, Studies, and Reports"]) {
+  ## Google Drive deauthorisation
+  googledrive::drive_deauth()
+
+  ## Get id
+  x <- bangsa_ls(id = id)
+
+  ## Return x
+  return(x)
+}
+
+
+################################################################################
+#
+#'
+#' Get the OpenBangsamoro Database inventory
+#'
+#' @param dataset A string value from either `"geospatial"`, `"tabular"`, or
+#'   `"docs"` specifying the inventory to retrieve.
+#'
+#' @return A tibble of the specified inventory of various datasets available in
+#'   the OpenBangsamoro Database.
+#'
+#' @author Ernest Guevarra
+#'
+#' @examples
+#' ## Retrieve the geospatial datasets inventory
+#' bangsa_get_inventory(dataset = "geospatial")
+#'
+#' @export
+#'
+#
+################################################################################
+
+bangsa_get_inventory <- function(dataset = c("geospatial", "tabular", "docs")) {
+  ## googlesheets deauthorise
+  googlesheets4::gs4_deauth()
+
+  ## Define dataset argument
+  dataset <- match.arg(dataset)
+
+  sheet <- switch(EXPR = dataset,
+                  geospatial = 1,
+                  tabular = 2,
+                  docs = 3)
+
+  ## Read the specified inventory
+  x <- googlesheets4::read_sheet(ss = "1599Dm3Csc1p328Y4HPgiVDSh3KAJi18rzVd5VXojm9Q",
+                                 sheet = sheet)
+
+  ## Return x
+  return(x)
+}
+
+
+################################################################################
+#
+#'
+#' Get the OpenBangsamoro Database metadata
+#'
+#' @return A tibble containing metadata information regarding the
+#'   OpenBangsamoro Database.
+#'
+#' @author Ernest Guevarra
+#'
+#' @examples
+#' bangsa_get_metadata()
+#'
+#' @export
+#'
+#
+################################################################################
+
+bangsa_get_metadata <- function() {
+  ## googlesheets deauthorise
+  googlesheets4::gs4_deauth()
+
+  ## Read the specified inventory
+  x <- googlesheets4::read_sheet(ss = "1599Dm3Csc1p328Y4HPgiVDSh3KAJi18rzVd5VXojm9Q",
+                                 sheet = "Metadata", skip = 1)
+
+  ## Return x
+  return(x)
+}
 
 
